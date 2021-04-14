00a1809217d8aa996bd4cce6990d4cd523f7ae03
Joe Worsfold <joseph.worsfold@digital.homeoffice.gov.uk>
2021-04-14 13:46:51 +0100

    implemented DELETE

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,4 +31,12 @@ public class StudentService {
         }
         studentRepository.save(student);
     }
+
+    public void deleteStudent(Long studentId) {
+        if (!studentRepository.existsById(studentId)) {
+            throw  new IllegalStateException("Student with ID " + studentId + " does not exist.");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
 }
